4091390a6713a7f27023f63161de25af3acb80cb
Francisco Sabino <fdasabino@gmail.com>
2021-08-13 01:36:17 +0200

    first commit

---
+++ student.py
@@ -0,0 +1,24 @@
+from datetime import date, timedelta
+
+class Student:
+    """ A Student class as a base for method testing """
+    def __init__(self, first_name, last_name):
+        self._first_name  = first_name
+        self._last_name = last_name
+        self._start_date = date.today()
+        self.end_date = date.today() + timedelta(days=365)
+        self.naughty_list = False
+
+    @property # read only
+    def full_name(self):
+        return f"{self._first_name} {self._last_name}"
+
+    @property # read only
+    def email(self):
+        return f"{self._first_name.lower()}.{self._last_name.lower()}@email.com"
+
+    def alert_santa(self):
+        self.naughty_list = True
+
+    def apply_extension(self, days):
+        self.end_date += timedelta(days=days)
